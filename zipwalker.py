79c1142b8f02c453391a5272582f2772c095e269
Kevin - Stark 4N6 <48143894+stark4n6@users.noreply.github.com>
2024-10-30 13:52:27 -0400

    Update zipwalker.py

--- zipwalker.py
+++ zipwalker.py
@@ -14,10 +14,14 @@ ascii_art = r'''
     /____|_| .__/ \_/\_/ \__,_|_|_|\_\___|_|   
            |_|
 
-               ZipWalker v0.0.1
+               ZipWalker v0.0.2
       https://github.com/stark4n6/ZipWalker
  @KevinPagano3 | @stark4n6 | startme.stark4n6.com
 '''
+
+splitter = '\\'
+global count
+count = 0
 
 def is_platform_windows():
     '''Returns True if running on Windows'''
@@ -49,46 +53,34 @@ def decode_extended_timestamp(extra_data):
         else:
             offset += data_size
     return None
-
-def main(zip_path,export_path):
     
-    print(ascii_art)
-    print()
-    
-    start_time = time.time()
-    print('Start: ' + str(datetime.datetime.now()))
-    print('Source: ' + zip_path)
-    print('Destination: ' + export_path)
-    
-    base = "ZipWalker_Out_"
-    
-    if is_platform_windows():
-        if zip_path[1] == ':': zip_path = '\\\\?\\' + zip_path.replace('/', '\\')
-        if export_path[1] == ':': export_path = '\\\\?\\' + export_path.replace('/', '\\')
+def check_input(input_path,out_folder,count):
+    if os.path.isdir(input_path):
+        for root, dirs, files in os.walk(input_path):
+            for file in files:
+                if file.endswith(".zip"):
+                    zip_file_path = os.path.join(root, file)
+                    print(f"Found ZIP file: {zip_file_path}")
+                    process_input(zip_file_path,out_folder,count)
+                    count += 1
         
-        if not export_path.endswith('\\'):
-            export_path = export_path + '\\'
-    
-    platform = is_platform_windows()
-    if platform:
-        splitter = '\\'
+    elif zipfile.is_zipfile(input_path):
+        print(f"Processing ZIP file: {input_path}")
+        process_input(input_path,out_folder,count)
     else:
-        splitter = '/' 
-    
-    output_ts = time.strftime("%Y%m%d-%H%M%S")
-    out_folder = export_path + base + output_ts
-    os.makedirs(out_folder)
-    
+        print("Unknown input type, please make sure your input contains ZIP files.")
+  
+def process_input(input_path,out_folder,count):
     try:
-        db_file_path = out_folder + splitter + 'file_listing.db'
-        with zipfile.ZipFile(zip_path, mode="r") as archive, sqlite3.connect(db_file_path) as conn:
+        db_file_path = out_folder + splitter + str(count) + '-' + os.path.basename(input_path) + '_file_listing.db'
+        with zipfile.ZipFile(input_path, mode="r") as archive, sqlite3.connect(db_file_path) as conn:
             
             cursor = conn.cursor()
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS file_listing (
                     file_name TEXT,
                     file_extension TEXT,
-                    path TEXT PRIMARY KEY,
+                    entry_path TEXT COLLATE NOCASE PRIMARY KEY,
                     created_date TEXT,
                     modified_date TEXT,
                     accessed_date TEXT,
@@ -99,7 +91,7 @@ def main(zip_path,export_path):
             ''')
 
             for info in archive.infolist():
-                path = info.filename
+                entry_path = info.filename
                 
                 size = info.file_size
                 comp_size = info.compress_size
@@ -120,25 +112,58 @@ def main(zip_path,export_path):
                 else:
                     modified_date = ''
                 
-                is_file = 1 if not path.endswith('/') else 0  # Check if it's a file
-                file_name = os.path.basename(path) if is_file else None
+                is_file = 1 if not entry_path.endswith('/') else 0  # Check if it's a file
+                file_name = os.path.basename(entry_path) if is_file else None
                 file_extension = os.path.splitext(file_name)[1] if is_file else None
                 
-                cursor.execute("INSERT INTO file_listing (file_name, file_extension, path, created_date, modified_date, accessed_date, is_file, size, comp_size) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                           (file_name, file_extension, path, created_date, modified_date, accessed_date, is_file, size, comp_size))
+                cursor.execute("INSERT OR IGNORE INTO file_listing (file_name, file_extension, entry_path, created_date, modified_date, accessed_date, is_file, size, comp_size) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                           (file_name, file_extension, entry_path, created_date, modified_date, accessed_date, is_file, size, comp_size))
 
         conn.commit()
 
     except FileNotFoundError:
-        print(f"ZIP file '{zip_path}' not found.")
+        print(f"ZIP file '{input_path}' not found.")
+        
+def main(input_path,export_path):
+    
+    print(ascii_art)
+    print()
+    
+    start_time = time.time()
+    print('Start: ' + str(datetime.datetime.now()))
+    print('Source: ' + input_path)
+    print('Destination: ' + export_path)
+    print()
+    
+    base = "ZipWalker_Out_"
+    
+    if is_platform_windows():
+        if input_path[1] == ':': input_path = '\\\\?\\' + input_path.replace('/', '\\')
+        if export_path[1] == ':': export_path = '\\\\?\\' + export_path.replace('/', '\\')
+        
+        if not export_path.endswith('\\'):
+            export_path = export_path + '\\'
+    
+    platform = is_platform_windows()
+    if platform:
+        splitter = '\\'
+    else:
+        splitter = '/' 
+    
+    output_ts = time.strftime("%Y%m%d-%H%M%S")
+    out_folder = export_path + base + output_ts
+    os.makedirs(out_folder)
+    
+    check_input(input_path,out_folder,count)
         
     print()
     print('****JOB FINISHED****')
     print('Runtime: %s seconds' % (time.time() - start_time))
+    print('ZIP files processed: ' + str(count))
         
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="ZipWalker v0.0.1 by @KevinPagano3 | @stark4n6 | https://github.com/stark4n6/ZipWalker")
-    parser.add_argument("zip_path", help="Path to the ZIP file")
+    parser = argparse.ArgumentParser(description="ZipWalker v0.0.2 by @KevinPagano3 | @stark4n6 | https://github.com/stark4n6/ZipWalker")
+    parser.add_argument("input_path", help="Path to the ZIP file or folder containing ZIP files")
     parser.add_argument("export_path", help="Path for the export report")
     args = parser.parse_args()
-    main(args.zip_path,args.export_path)
+    main(args.input_path,args.export_path)
